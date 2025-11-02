8cdcc601a2d6d03c5162bf01899d26561e3d9447
guiv42 <129443524+guiv42@users.noreply.github.com>
2025-11-02 22:36:24 +0100

    update to 2.0.0

--- tuxguitar.sh
+++ tuxguitar.sh
@@ -8,12 +8,10 @@ CLASSPATH=${CLASSPATH}:${TG_DIR}/lib/*
 CLASSPATH=${CLASSPATH}:${TG_DIR}/share/
 CLASSPATH=${CLASSPATH}:${TG_DIR}/dist/
 ##MAINCLASS
-MAINCLASS=org.herac.tuxguitar.app.TGMainSingleton
-##JVM ARGUMENTS
-VM_ARGS="-Xmx512m"
+MAINCLASS=app.tuxguitar.app.TGMainSingleton
 ##EXPORT VARS
 export CLASSPATH
 export LD_LIBRARY_PATH
 export LV2_PATH=${LV2_PATH:-$HOME/.lv2:/app/extensions/Plugins/lv2:/app/lib/lv2}
 ##LAUNCH
-${JAVA} ${VM_ARGS} -cp :${CLASSPATH} -Dtuxguitar.home.path="${TG_DIR}" -Dtuxguitar.share.path="share/" -Djava.library.path="${LD_LIBRARY_PATH}" ${MAINCLASS} "$@"
+${JAVA} -cp :${CLASSPATH} -Dtuxguitar.home.path="${TG_DIR}" -Dtuxguitar.share.path="share/" -Djava.library.path="${LD_LIBRARY_PATH}" ${MAINCLASS} "$@"
