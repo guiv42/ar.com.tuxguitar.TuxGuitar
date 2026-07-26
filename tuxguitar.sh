11c6dc6c932901af7c5a6a1b40cb48e6bd3b68fe
guiv42 <129443524+guiv42@users.noreply.github.com>
2026-07-26 14:27:27 +0200

    update to 2.1.0

--- tuxguitar.sh
+++ tuxguitar.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+## try to read light/dark theme from dbus
+getTheme() {
+    tuxguitarTheme="none"
+    # does not work with all desktops, limited to KDE
+    if [ "$XDG_CURRENT_DESKTOP" = 'KDE' ]; then
+        dbusTheme=$(dbus-send --session --print-reply=literal --dest=org.freedesktop.portal.Desktop /org/freedesktop/portal/desktop org.freedesktop.portal.Settings.Read string:org.freedesktop.appearance string:color-scheme 2>/dev/null)
+        [ -n "$( echo $dbusTheme | sed -n '/uint32 2/p')" ] && tuxguitarTheme="light"
+        [ -n "$( echo $dbusTheme | sed -n '/uint32 1/p')" ] && tuxguitarTheme="dark"
+    fi
+    echo $tuxguitarTheme
+}
+
 ##SCRIPT DIR
 TG_DIR=/app
 ##LIBRARY_PATH
@@ -14,4 +26,4 @@ export CLASSPATH
 export LD_LIBRARY_PATH
 export LV2_PATH=${LV2_PATH:-$HOME/.lv2:/app/extensions/Plugins/lv2:/app/lib/lv2}
 ##LAUNCH
-${JAVA} -cp :${CLASSPATH} -Dtuxguitar.home.path="${TG_DIR}" -Dtuxguitar.share.path="share/" -Djava.library.path="${LD_LIBRARY_PATH}" ${MAINCLASS} "$@"
+${JAVA} -cp ":${CLASSPATH}" -Dtuxguitar.home.path="${TG_DIR}" -Dtuxguitar.share.path="share/" -Djava.library.path="${LD_LIBRARY_PATH}" -Dtuxguitar.theme=$(getTheme) ${MAINCLASS} "$@"
